d4d01547659d7834ce90c5c94882df8f9dee5523
Edson Cândido <edson.c.a.j@hotmail.com>
2025-01-14 23:41:47 -0300

    [feat]

--- src/components/admin/Clientes/ModalCliente/index.tsx
+++ src/components/admin/Clientes/ModalCliente/index.tsx
@@ -1,6 +1,18 @@
-import { Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, Flex } from "@chakra-ui/react";
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import Loading from "../../../Loading";
+import { http } from "../../../../services/http";
+import { toast } from "react-toastify";
 
 type input = {
   clientId: number | null;
@@ -10,20 +22,36 @@ type input = {
 };
 const ModalCliente = ({ clientId, isOpen, onClose, onSuccess }: input) => {
   const [isLoadingPage, setIsLoadingPage] = useState(false);
-
+  const [isLoadingSubmit, setIsLoadingSubmit] = useState(false);
   const onCloseModal = () => {
     void onClose();
   };
   const onSubmit = async () => {
+    setIsLoadingSubmit(true);
 
-  }
+    const request = await http
+      .post("/clients", {})
+      .then((e) => ({ data: e.data, success: true, message: "OK" }))
+      .catch((err) => ({
+        data: null,
+        success: false,
+        message: err.response.data,
+      }));
+
+    if (request.success) {
+      void onCloseModal();
+      void onSuccess();
+    }else{
+        toast.warn(request.message, {position: 'top-left'});
+    }
+  };
 
   const onInit = async () => {
     setIsLoadingPage(true);
     if (clientId) {
-        /**
-         * TODO pesquisa cliente por id
-         */
+      /**
+       * TODO pesquisa cliente por id
+       */
     }
 
     setTimeout(() => {
@@ -51,10 +79,26 @@ const ModalCliente = ({ clientId, isOpen, onClose, onSuccess }: input) => {
           {clientId ? "Editar Cliente" : "Novo Cliente"}
         </DrawerHeader>
         <DrawerBody>
-            <Flex w={'100%'}>
-                <Loading/>
-            </Flex>
+          <Flex w={"100%"}>
+            <Loading />
+          </Flex>
         </DrawerBody>
+        <DrawerFooter>
+          {!isLoadingPage && (
+            <>
+              <Button variant="outline" mr={3} onClick={onCloseModal}>
+                Cancelar
+              </Button>
+              <Button
+                colorScheme="blue"
+                onClick={onSubmit}
+                isDisabled={isLoadingSubmit}
+              >
+                Salvar
+              </Button>
+            </>
+          )}
+        </DrawerFooter>
       </DrawerContent>
     </Drawer>
   );
