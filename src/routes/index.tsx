4dd01c49548e43fa2c60006176b9ebc71d16da3e
Edson Cândido <edson.c.a.j@hotmail.com>
2025-02-23 23:25:19 -0300

    [feat] financial router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,95 +7,126 @@ import LoadingPage from "../view/loading";
 import NotFound from "../view/not-found";
 import DashboardLayout from "../layout/dashboardLayout";
 
-
 const AdminSettingslazy = lazy(() => import("../view/admin/settings"));
-const UsersAdminLazy = lazy(() => import('../view/admin/users'));
-const BannersAdminLazy = lazy(() => import('../view/admin/banners'));
-const ServicesAdminLazy = lazy(() => import('../view/admin/services'));
-const NoticiasAdminLazy = lazy(() => import('../view/admin/noticias'));
-const DocumentosAdminLazy = lazy(() => import('../view/admin/documentos'));
-const ClientesAdminLazy = lazy(() => import('../view/admin/clientes'));
+const UsersAdminLazy = lazy(() => import("../view/admin/users"));
+const BannersAdminLazy = lazy(() => import("../view/admin/banners"));
+const ServicesAdminLazy = lazy(() => import("../view/admin/services"));
+const NoticiasAdminLazy = lazy(() => import("../view/admin/noticias"));
+const DocumentosAdminLazy = lazy(() => import("../view/admin/documentos"));
+const ClientesAdminLazy = lazy(() => import("../view/admin/clientes"));
+
+const FinancialPageLazy = lazy(() => import("../view/financial"));
 
 type IProps = {
-    isAdmin?: boolean;
-    children: JSX.Element;
-}
+  isAdmin?: boolean;
+  children: JSX.Element;
+};
 
 const RoutesAplication = () => {
-    const { user, signOut } = useAuth();
+  const { user, signOut } = useAuth();
 
-    const Autenticate = ({ children, isAdmin }: IProps) => {
-        let errAccess = false;
-        if (!user) errAccess = true;
+  const Autenticate = ({ children, isAdmin }: IProps) => {
+    let errAccess = false;
+    if (!user) errAccess = true;
 
-        if (isAdmin && user?.is_admin === 0) errAccess = true;
+    if (isAdmin && user?.is_admin === 0) errAccess = true;
 
-        if (errAccess) {
-            signOut();
-            return <Navigate to="/login" replace />;
-        }
-        return children
-    };
+    if (errAccess) {
+      signOut();
+      return <Navigate to="/login" replace />;
+    }
+    return children;
+  };
 
-
-    return (
-        <Routes>
-            <Route path='/' element={<Navigate to='/login' replace />} />
-            <Route path='/login' element={<LoginPage />} />
-            <Route element={<DashboardLayout />}>
-                <Route path='/admin/settings' element={
-                    <Autenticate isAdmin>
-                        <Suspense fallback={<LoadingPage />}>
-                            <AdminSettingslazy />
-                        </Suspense>
-                    </Autenticate>
-                } />
-                <Route path='/admin/settings/users' element={
-                    <Autenticate isAdmin>
-                        <Suspense fallback={<LoadingPage />}>
-                            <UsersAdminLazy />
-                        </Suspense>
-                    </Autenticate>
-                } />
-                <Route path='/admin/settings/documentos' element={
-                    <Autenticate isAdmin>
-                        <Suspense fallback={<LoadingPage />}>
-                            <DocumentosAdminLazy />
-                        </Suspense>
-                    </Autenticate>
-                } />
-                <Route path='/admin/settings/clientes' element={
-                    <Autenticate isAdmin>
-                        <Suspense fallback={<LoadingPage />}>
-                            <ClientesAdminLazy />
-                        </Suspense>
-                    </Autenticate>
-                } />
-                <Route path='/admin/settings/banners' element={
-                    <Autenticate isAdmin>
-                        <Suspense fallback={<LoadingPage />}>
-                            <BannersAdminLazy />
-                        </Suspense>
-                    </Autenticate>
-                } />
-                <Route path='/admin/settings/services' element={
-                    <Autenticate isAdmin>
-                        <Suspense fallback={<LoadingPage />}>
-                            <ServicesAdminLazy />
-                        </Suspense>
-                    </Autenticate>
-                } />
-                <Route path='/admin/settings/noticias' element={
-                    <Autenticate isAdmin>
-                        <Suspense fallback={<LoadingPage />}>
-                            <NoticiasAdminLazy />
-                        </Suspense>
-                    </Autenticate>
-                } />
-            </Route>
-            <Route path='*' element={<NotFound />} />
-        </Routes>
-    );
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route element={<DashboardLayout />}>
+        <Route
+          path="/financial"
+          element={
+            <Autenticate isAdmin>
+              <Suspense fallback={<LoadingPage />}>
+                <FinancialPageLazy />
+              </Suspense>
+            </Autenticate>
+          }
+        />
+        <Route
+          path="/admin/settings"
+          element={
+            <Autenticate isAdmin>
+              <Suspense fallback={<LoadingPage />}>
+                <AdminSettingslazy />
+              </Suspense>
+            </Autenticate>
+          }
+        />
+        <Route
+          path="/admin/settings/users"
+          element={
+            <Autenticate isAdmin>
+              <Suspense fallback={<LoadingPage />}>
+                <UsersAdminLazy />
+              </Suspense>
+            </Autenticate>
+          }
+        />
+        <Route
+          path="/admin/settings/documentos"
+          element={
+            <Autenticate isAdmin>
+              <Suspense fallback={<LoadingPage />}>
+                <DocumentosAdminLazy />
+              </Suspense>
+            </Autenticate>
+          }
+        />
+        <Route
+          path="/admin/settings/clientes"
+          element={
+            <Autenticate isAdmin>
+              <Suspense fallback={<LoadingPage />}>
+                <ClientesAdminLazy />
+              </Suspense>
+            </Autenticate>
+          }
+        />
+        <Route
+          path="/admin/settings/banners"
+          element={
+            <Autenticate isAdmin>
+              <Suspense fallback={<LoadingPage />}>
+                <BannersAdminLazy />
+              </Suspense>
+            </Autenticate>
+          }
+        />
+        <Route
+          path="/admin/settings/services"
+          element={
+            <Autenticate isAdmin>
+              <Suspense fallback={<LoadingPage />}>
+                <ServicesAdminLazy />
+              </Suspense>
+            </Autenticate>
+          }
+        />
+        <Route
+          path="/admin/settings/noticias"
+          element={
+            <Autenticate isAdmin>
+              <Suspense fallback={<LoadingPage />}>
+                <NoticiasAdminLazy />
+              </Suspense>
+            </Autenticate>
+          }
+        />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
 
 export default RoutesAplication;
